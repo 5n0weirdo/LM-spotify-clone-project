b6d2d65b885843d5c35b82aa08ae9670e29c2dfc
5n0weirdo <snoweirdo@protonmail.com>
2021-11-30 22:29:45 +0200

    Get user details logged in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Login from "./Login";
 import React, { useEffect, useState } from "react";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -14,6 +17,12 @@ function App() {
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("Me Account", user);
+      });
     }
 
     console.log("I have a token", token);
