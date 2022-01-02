35c186e1b66f8407e2ec38ea279e307251a0728a
5n0weirdo <snoweirdo@protonmail.com>
2022-01-02 13:16:36 +0200

    Start login

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div>
+      {/* Spotify logo */}
+      {/*  */}
+
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
