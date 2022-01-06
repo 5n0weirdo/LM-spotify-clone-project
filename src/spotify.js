7e2470ca3498e0f20550dbccffcc1b74fe7e4ab6
5n0weirdo <snoweirdo@protonmail.com>
2022-01-06 16:59:04 +0200

    Created spotify variables

---
+++ src/spotify.js
@@ -0,0 +1,17 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "854ead57a6284c649e59de9e1ba3acf8";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-rated",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
