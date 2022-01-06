a0e9aa8b2f7e0f6b84f91e0c91f0ba73a238d18f
5n0weirdo <snoweirdo@protonmail.com>
2022-01-06 17:23:19 +0200

    Fixed invalid client and illegal scope error

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "http://localhost:3000/callback";
 
 const clientId = "854ead57a6284c649e59de9e1ba3acf8";
 
@@ -8,7 +8,6 @@ const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
   "user-read-playback-state",
-  "user-top-rated",
   "user-modify-playback-state",
 ];
 
